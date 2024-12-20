625eeeb79b23f0da81f2b4ae3847b744fd36268d
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2024-12-20 15:27:24 +0100

    Added loading assemblies (not unloadable yet)

---
+++ MiniTest/MiniTestRunner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Reflection.Metadata;
+using System.Runtime.Loader;
+
+namespace MiniTestRunner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(arg);
+                    if (AssemblyLoadContext.GetLoadContext(assembly) is null)
+                    {
+                        continue;
+                    }
+                    AssemblyLoadContext context = AssemblyLoadContext.GetLoadContext(assembly)!;
+                }
+                catch(FileNotFoundException)
+                {
+                    Console.WriteLine($"{arg}: File not found");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{arg}: An error occured when loading the file");
+                }
+            }
+        }
+    }
+}
