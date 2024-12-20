2c2122cc75ecf523ad2cc85c0520b225340bebea
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2024-12-20 23:46:24 +0100

    Test Discovery progress

--- MiniTest/MiniTestRunner/Program.cs
+++ MiniTest/MiniTestRunner/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using MiniTest;
+using System.Diagnostics;
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Runtime.Loader;
@@ -16,8 +17,30 @@ namespace MiniTestRunner
                 try
                 {
                     Assembly assembly = context.LoadFromAssemblyPath(arg);
+
+                    var testClass = assembly.GetTypes()
+                        .Where(type => type.IsClass
+                            && type.GetCustomAttribute(typeof(MiniTest.TestClassAttribute)) != null
+                            && type.GetConstructors().Any(ctor => ctor.GetParameters().Length == 0)
+                        );
+
+                    var beforeEachMethod = testClass.GetType().GetMethods()
+                        .Where(method => method.GetCustomAttribute(typeof(MiniTest.BeforeEachAttribute)) != null);
+
+                    var afterEachMethod = testClass.GetType().GetMethods()
+                        .Where(method => method.GetCustomAttribute(typeof(MiniTest.BeforeEachAttribute)) != null);
+
+                    //TODO: Late bind BeforeEach and AfterEach methods to a delegate
+
+                    var testMethods = testClass.GetType().GetMethods()
+                        .Where(method => method.GetCustomAttribute(typeof(MiniTest.TestMethodAttribute)) != null);
+
+                    var parametrizedTests = testMethods
+                        .Where(method => method.GetCustomAttributes(typeof(MiniTest.DataRowAttribute)) != null);
+
+                    //TODO: Ignore test methods or attributes with incompatible configurations (e.g., parameter mismatch for DataRow).
                 }
-                catch(FileNotFoundException)
+                catch (FileNotFoundException)
                 {
                     Console.WriteLine($"{arg}: File not found");
                 }
