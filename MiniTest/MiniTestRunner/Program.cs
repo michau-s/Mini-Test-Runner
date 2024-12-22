c8fbd274b2152620cf5b259c65e9f826681f27ed
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2024-12-22 19:16:04 +0100

    Test Discovery - now properly discovering methods

--- MiniTest/MiniTestRunner/Program.cs
+++ MiniTest/MiniTestRunner/Program.cs
@@ -12,12 +12,25 @@ namespace MiniTestRunner
         {
             AssemblyLoadContext context = new AssemblyLoadContext("assembly", isCollectible: true);
 
-            //TODO: write actualy functions like a normal person and put everything out of main
+            //TODO: write actuall functions like a normal person and put everything out of main
 
             foreach (var arg in args)
             {
                 try
                 {
+                    // Apparently it does not work without this resolver
+                    AssemblyDependencyResolver resolver = new AssemblyDependencyResolver(arg);
+
+                    context.Resolving += (context, assemblyName) =>
+                    {
+                        string? resolvedPath = resolver.ResolveAssemblyToPath(assemblyName);
+                        if (resolvedPath != null)
+                        {
+                            return context.LoadFromAssemblyPath(resolvedPath);
+                        }
+                        return null;
+                    };
+
                     Assembly assembly = context.LoadFromAssemblyPath(arg);
 
                     var testClass = assembly.GetTypes()
@@ -34,16 +47,24 @@ namespace MiniTestRunner
                             continue;
 
                         var beforeEachMethod = instance.GetType().GetMethods()
-                            .Where(method => method.GetCustomAttribute(typeof(MiniTest.BeforeEachAttribute)) != null).Single();
+                            .Where(method => method.GetCustomAttribute(typeof(MiniTest.BeforeEachAttribute)) != null).FirstOrDefault();
 
                         var afterEachMethod = instance.GetType().GetMethods()
-                            .Where(method => method.GetCustomAttribute(typeof(MiniTest.AfterEachAttribute)) != null).Single();
+                            .Where(method => method.GetCustomAttribute(typeof(MiniTest.AfterEachAttribute)) != null).FirstOrDefault();
 
-                        Delegate before = Delegate.CreateDelegate(beforeEachMethod.GetType(), beforeEachMethod);
-                        Delegate after = Delegate.CreateDelegate(afterEachMethod.GetType(), afterEachMethod);
+
+                        Delegate? before = beforeEachMethod == null ? null : Delegate.CreateDelegate(typeof(Action), instance, beforeEachMethod);
+                        Delegate? after = afterEachMethod == null ? null :Delegate.CreateDelegate(typeof(Action), instance, afterEachMethod);
 
                         var testMethods = instance.GetType().GetMethods()
                             .Where(method => method.GetCustomAttribute(typeof(MiniTest.TestMethodAttribute)) != null);
+
+                        Console.WriteLine($"Class: {_class.Name}");
+
+                        foreach (var method in testMethods)
+                        {
+                            Console.WriteLine($"Method: {method.Name}");
+                        }
 
                         //TODO: Ignore test methods or attributes with incompatible configurations (e.g., parameter mismatch for DataRow).
                         //TODO: Write a warning message to the console in case of such configuration incompatibilities.
@@ -52,6 +73,8 @@ namespace MiniTestRunner
                         var parametrizedTests = testMethods
                             .Where(method => method.GetCustomAttributes(typeof(MiniTest.DataRowAttribute)) != null);
 
+
+                        //TODO: Fix methods without DataRow reporting parameter missmatch
                         foreach (var testMethod in testMethods)
                         {
                             var dataRows = testMethod.GetCustomAttributes(typeof(MiniTest.DataRowAttribute)).ToList();
@@ -62,14 +85,20 @@ namespace MiniTestRunner
                                 {
                                     try
                                     {
-                                        before.DynamicInvoke(instance);
+                                        if (before != null)
+                                        {
+                                            before.DynamicInvoke(instance);
+                                        }
 
                                         var testDataField = dataRow.GetType().GetField("testData");
                                         var parameters = testDataField?.GetValue(dataRow) as object[];
 
                                         testMethod.Invoke(instance, parameters);
 
-                                        after.DynamicInvoke(instance);
+                                        if (after != null)
+                                        {
+                                            after.DynamicInvoke(instance);
+                                        }
                                     }
                                     catch (Exception e)
                                     {
@@ -81,10 +110,17 @@ namespace MiniTestRunner
                             {
                                 try
                                 {
-                                    before.DynamicInvoke(instance);
+                                    if (before != null)
+                                    {
+                                        before.DynamicInvoke(instance);
+                                    }
 
                                     testMethod.Invoke(instance, null);
-                                    after.DynamicInvoke(instance);
+
+                                    if (after != null)
+                                    {
+                                        after.DynamicInvoke(instance);
+                                    }
                                 }
                                 catch (Exception e)
                                 {
