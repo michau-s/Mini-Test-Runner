573c0fedef08b4b2c69eb5f82942bf7f45c4e57e
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2024-12-22 16:37:20 +0100

    Test Execution progress

--- MiniTest/MiniTestRunner/Program.cs
+++ MiniTest/MiniTestRunner/Program.cs
@@ -12,6 +12,8 @@ namespace MiniTestRunner
         {
             AssemblyLoadContext context = new AssemblyLoadContext("assembly", isCollectible: true);
 
+            //TODO: write actualy functions like a normal person and put everything out of main
+
             foreach (var arg in args)
             {
                 try
@@ -44,9 +46,52 @@ namespace MiniTestRunner
                             .Where(method => method.GetCustomAttribute(typeof(MiniTest.TestMethodAttribute)) != null);
 
                         //TODO: Ignore test methods or attributes with incompatible configurations (e.g., parameter mismatch for DataRow).
+                        //TODO: Write a warning message to the console in case of such configuration incompatibilities.
 
+                        //I don't think I'll use this actually
                         var parametrizedTests = testMethods
                             .Where(method => method.GetCustomAttributes(typeof(MiniTest.DataRowAttribute)) != null);
+
+                        foreach (var testMethod in testMethods)
+                        {
+                            var dataRows = testMethod.GetCustomAttributes(typeof(MiniTest.DataRowAttribute)).ToList();
+
+                            if (dataRows.Count != 0)
+                            {
+                                foreach (var dataRow in dataRows)
+                                {
+                                    try
+                                    {
+                                        before.DynamicInvoke(instance);
+
+                                        var testDataField = dataRow.GetType().GetField("testData");
+                                        var parameters = testDataField?.GetValue(dataRow) as object[];
+
+                                        testMethod.Invoke(instance, parameters);
+
+                                        after.DynamicInvoke(instance);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine($"test {testMethod.Name} failed: {e.Message}");
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    before.DynamicInvoke(instance);
+
+                                    testMethod.Invoke(instance, null);
+                                    after.DynamicInvoke(instance);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine($"Test {testMethod.Name} failed: {e.Message}");
+                                }
+                            }
+                        }
                     }
                 }
                 catch (FileNotFoundException)
